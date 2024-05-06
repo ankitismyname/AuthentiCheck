3b15613db4994101301959f04d46f17569ff5691
Ankit Meena <120910719+ankitismyname@users.noreply.github.com>
2024-05-06 15:12:04 +0530

    Add files via upload

--- contracts/Central.sol
+++ contracts/Central.sol
@@ -1,110 +1,196 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./Company.sol";
+import "./Storage.sol";
 
 contract Central {
+    struct AllInfo {
+        string hashCode;
+        string manufacturer;
+        string seller;
+        address sellerAddress;
+        uint256 manufacturedTime;
+        uint256 registeredTime;
+    }
     struct VotingMember {
         address walletAddress;
+        string nominatedBy;
         string username;
         uint256 yesVotes;
         uint256 noVotes;
-    }
-    struct ContractInfo {
-        address contractAddress;
-        string productName;
+        bool isManufacturer;
     }
 
     struct WalletVote {
+        bool istrue;
+        mapping(address => bool) voted;
+        bool isOpen;
+        string username;
         uint256 yesVotes;
         uint256 noVotes;
-        mapping(address => bool) voted;
-        bool isOpen; // Added isOpen flag
+        bool isManufacturer;
+        string nominateBy;
     }
 
-    struct Wallet {
-        address walletAddress;
+    struct SupplierInfo {
         string username;
+        address addressInfo;
     }
 
-    Wallet[] public AllExistingMembers;
-    Wallet[] public VotingMembersInfoArray;
-    mapping(address => ContractInfo[]) public InfoOfProductAndContractAddress;
-    mapping(address => WalletVote) private walletVotes;
+    struct OrganizationInfo {
+        string username;
+        address organizationContract;
+    }
 
-    uint256 public totalExistingWallets;
+    struct ProductInfo {
+        string productName;
+        address contractAddress;
+    }
 
+    struct Info {
+        bool isTrue;
+        uint256 index;
+    }
+
+    mapping(string => address) hashCodeToContract;
+    AllInfo[] allProductsInfo;
+    mapping(string => Info) fastAccess;
+    mapping(address => ProductInfo[]) manufacturerProducts;
+    mapping(address => Info) manufacturerExists;
+    mapping(address => Info) supplierExists;
+    SupplierInfo[] allSuppliers;
+    OrganizationInfo[] allManufacturers;
+    mapping(address => WalletVote) walletVotes;
+
+    address[] votingMembersInfoArray;
+
+    uint256 totalManufacturers;
     constructor() {
-        AllExistingMembers.push(Wallet(msg.sender, "Entangle Productions Limited"));
-        totalExistingWallets = 1; // Initialize with deployer's address
+        allManufacturers.push(
+            OrganizationInfo("Ankit Real Estate Pvt. Limited", msg.sender)
+        );
+        manufacturerExists[msg.sender] = Info(true, 0);
+        totalManufacturers = 1;
     }
 
-    modifier onlyExistingWallet(address _walletAddress) {
-        require(Iswalletexits(_walletAddress), "Wallet address does not exist");
+    modifier onlyManufacturer(address _walletAddress) {
+        require(
+            IsManufracturer(_walletAddress),
+            "Wallet address not a manufacturer"
+        );
         _;
     }
 
-    function Iswalletexits(address _walletAddress) public view returns (bool) {
-        for (uint256 i = 0; i < totalExistingWallets; i++) {
-            if (AllExistingMembers[i].walletAddress == _walletAddress) {
-                return true;
-            }
+    modifier onlySuppliers(address _walletAddress) {
+        require(IsSupplier(_walletAddress), "Wallet address not a supplier");
+        _;
+    }
+    function IsSupplier(address _walletaddress) public view returns (bool) {
+        return supplierExists[_walletaddress].isTrue;
+    }
+    function IsManufracturer(
+        address _walletaddress
+    ) public view returns (bool) {
+        return manufacturerExists[_walletaddress].isTrue;
+    }
+
+    modifier onlyVerifiedProducts(
+        string memory _productHashCode,
+        address _productContractAddressSupplier
+    ) {
+        require(
+            verifyProductBySupplier(_productHashCode),
+            "Product is invalid"
+        );
+        _;
+    }
+
+    function createManufacturerProductContract(
+        string memory _productName
+    ) public onlyManufacturer(msg.sender) {
+        Storage productContract = new Storage();
+        manufacturerProducts[msg.sender].push(
+            ProductInfo(_productName, address(productContract))
+        );
+    }
+
+    function addProductByManufacturer(
+        string memory _hashCode,
+        address _productContractAddress
+    ) public onlyManufacturer(msg.sender) {
+        require(
+            _productContractAddress != address(0),
+            "Invalid contract address"
+        );
+
+        Storage productInstance = Storage(_productContractAddress);
+        require(
+            address(productInstance) != address(0),
+            "Invalid contract instance"
+        );
+
+        hashCodeToContract[_hashCode] = _productContractAddress;
+        productInstance.addProduct(_hashCode);
+    }
+
+    function verifyProductBySupplier(
+        string memory _productHashCode
+    ) public view onlySuppliers(msg.sender) returns (bool) {
+        Storage productInstance = Storage(hashCodeToContract[_productHashCode]);
+        return productInstance.verifyProduct(_productHashCode);
+    }
+
+    function addProductBySupplier(
+        string memory _hashCode
+    ) public onlyVerifiedProducts(_hashCode, hashCodeToContract[_hashCode]) {
+        require(
+            !fastAccess[_hashCode].isTrue,
+            "Product already reached to a supplier"
+        );
+        address contractAddress = hashCodeToContract[_hashCode];
+        fastAccess[_hashCode].isTrue = true;
+        Storage productInstance = Storage(contractAddress);
+        allProductsInfo.push(
+            AllInfo(
+                _hashCode,
+                allManufacturers[
+                    manufacturerExists[productInstance.getowner()].index
+                ].username,
+                allSuppliers[supplierExists[msg.sender].index].username,
+                allSuppliers[supplierExists[msg.sender].index].addressInfo,
+                productInstance.gettimestamp(_hashCode),
+                block.timestamp
+            )
+        );
+        fastAccess[_hashCode] = Info(true, allProductsInfo.length - 1);
+    }
+
+    function verifyProductByCustomer(
+        string memory _productHashCode
+    ) public view returns (AllInfo memory) {
+        if (fastAccess[_productHashCode].isTrue) {
+            return allProductsInfo[fastAccess[_productHashCode].index];
+        } else {
+            revert("Product not found");
         }
-        return false;
     }
 
-    function IsThisAlreadyNominated(address _walletAddress) public view returns (bool) {
-        for (uint256 i = 0; i < VotingMembersInfoArray.length; i++) {
-            if (VotingMembersInfoArray[i].walletAddress == _walletAddress && walletVotes[_walletAddress].isOpen == true) {
-                return true;
-            }
-        }
-        return false;
-    }
-    function IsThisAvilableInVotingArray(address _walletAddress) public view returns (bool) {
-        for (uint256 i = 0; i < VotingMembersInfoArray.length; i++) {
-            if (VotingMembersInfoArray[i].walletAddress == _walletAddress) {
-                return true;
-            }
-        }
-        return false;
+    function getAllProductsInfo() public view returns (AllInfo[] memory) {
+        return allProductsInfo;
     }
 
-    function createSmartContract(address _walletAddress, string memory _productName) public onlyExistingWallet(_walletAddress) {
-        Company companyContract = new Company();
-        InfoOfProductAndContractAddress[msg.sender].push(ContractInfo(address(companyContract), _productName));
-    }
-
-
-
-        function addProduct(address _contractAddress, string memory _hashcode) public onlyExistingWallet(msg.sender) {
-        Company companyInstance = Company(_contractAddress);
-        companyInstance.addProduct(_hashcode,block.timestamp);
-    }
-
-    function verifyProduct(string memory _productHashCode) public view returns (bool) {
-    for (uint256 i = 0; i < AllExistingMembers.length; i++) {
-        Wallet storage wallet = AllExistingMembers[i];
-        for (uint256 j = 0; j < InfoOfProductAndContractAddress[wallet.walletAddress].length; j++) {
-            ContractInfo storage contractInfo = InfoOfProductAndContractAddress[wallet.walletAddress][j];
-            if (Company(contractInfo.contractAddress).verifyProduct(_productHashCode)) {
-                return true; // Product found
-            }
-        }
-    }
-    return false;
-    }
-
-    function getLastContractInfo(address _walletAddress) public view onlyExistingWallet(_walletAddress) returns (address contractAddress) {
-        ContractInfo[] storage contractInfoArray = InfoOfProductAndContractAddress[_walletAddress];
-        ContractInfo storage lastContractInfo = contractInfoArray[contractInfoArray.length - 1];
-        return (lastContractInfo.contractAddress);
-    }
-
-    function voteToAddWallet(address _newWalletAddress, bool _vote, string memory _username) public onlyExistingWallet(msg.sender) {
-        require(IsThisAlreadyNominated(_newWalletAddress), "Member is not part of voting");
-        require(walletVotes[_newWalletAddress].isOpen, "Voting is closed");
-        require(!walletVotes[_newWalletAddress].voted[msg.sender], "You already voted to nominate");
+    function voteToAddWallet(
+        address _newWalletAddress,
+        bool _vote
+    ) public onlyManufacturer(msg.sender) {
+        require(
+            walletVotes[_newWalletAddress].isOpen,
+            "Member is not part of voting"
+        );
+        require(
+            !walletVotes[_newWalletAddress].voted[msg.sender],
+            "You already voted to nominate"
+        );
 
         if (_vote) {
             walletVotes[_newWalletAddress].yesVotes += 1;
@@ -115,67 +201,134 @@ contract Central {
         walletVotes[_newWalletAddress].voted[msg.sender] = true;
 
         // Check if 50% or more votes are in favor to add the new wallet address
-        if (walletVotes[_newWalletAddress].yesVotes >= totalExistingWallets / 2 + 1) {
-            AllExistingMembers.push(Wallet(_newWalletAddress, _username));
-            totalExistingWallets += 1;
+        if (
+            walletVotes[_newWalletAddress].yesVotes >=
+            totalManufacturers / 2 + 1
+        ) {
+            if (walletVotes[_newWalletAddress].isManufacturer) {
+                allManufacturers.push(
+                    OrganizationInfo(
+                        walletVotes[_newWalletAddress].username,
+                        _newWalletAddress
+                    )
+                );
+                manufacturerExists[_newWalletAddress] = Info(
+                    true,
+                    allManufacturers.length - 1
+                );
+            } else {
+                allSuppliers.push(
+                    SupplierInfo(
+                        walletVotes[_newWalletAddress].username,
+                        address(_newWalletAddress)
+                    )
+                );
+                supplierExists[_newWalletAddress] = Info(
+                    true,
+                    allSuppliers.length - 1
+                );
+            }
             walletVotes[_newWalletAddress].isOpen = false; // Close voting
-        } else if (walletVotes[_newWalletAddress].noVotes >= totalExistingWallets / 2 + 1) {
+            for (uint256 i = 0; i < allManufacturers.length; i++) {
+                walletVotes[_newWalletAddress].voted[
+                    allManufacturers[i].organizationContract
+                ] = false;
+            }
+        } else if (
+            walletVotes[_newWalletAddress].noVotes >= totalManufacturers / 2 + 1
+        ) {
             walletVotes[_newWalletAddress].isOpen = false; // Close voting
+            for (uint256 i = 0; i < allManufacturers.length; i++) {
+                walletVotes[_newWalletAddress].voted[
+                    allManufacturers[i].organizationContract
+                ] = false;
+            }
         }
     }
 
-    function addMemberForVoting(address _newWalletAddress, string memory _newUsername) public onlyExistingWallet(msg.sender) {
-        require(!IsThisAlreadyNominated(_newWalletAddress), "Member already present for voting");
-        require(!Iswalletexits(_newWalletAddress), "Member already a partner");
-        if(!IsThisAvilableInVotingArray(_newWalletAddress)){
-        VotingMembersInfoArray.push(Wallet(_newWalletAddress, _newUsername));}
-        walletVotes[_newWalletAddress].isOpen = true; // Open voting
+    function addMemberForVoting(
+        address _newWalletAddress,
+        string memory _newUsername,
+        bool _isManufacturer
+    ) public onlyManufacturer(msg.sender) {
+        require(
+            !manufacturerExists[_newWalletAddress].isTrue &&
+                !supplierExists[_newWalletAddress].isTrue,
+            "Member already a partner"
+        );
+        require(
+            !walletVotes[_newWalletAddress].isOpen,
+            "Member already present for voting"
+        );
+        if (!walletVotes[_newWalletAddress].istrue) {
+            votingMembersInfoArray.push(_newWalletAddress);
+            walletVotes[_newWalletAddress].istrue = true;
+        }
+        walletVotes[_newWalletAddress].isOpen = true;
+        walletVotes[_newWalletAddress].nominateBy = allManufacturers[
+            manufacturerExists[msg.sender].index
+        ].username;
+        walletVotes[_newWalletAddress].username = _newUsername;
+        walletVotes[_newWalletAddress].isManufacturer = _isManufacturer;
         walletVotes[_newWalletAddress].yesVotes = 0;
         walletVotes[_newWalletAddress].noVotes = 0;
     }
 
-    
-
-    function getOpenVotingMembers() public view returns (VotingMember[] memory) {
-        uint256 count = 0;
-        for (uint256 i = 0; i < VotingMembersInfoArray.length; i++) {
-            if (walletVotes[VotingMembersInfoArray[i].walletAddress].isOpen) {
-                count++;
+    function getOpenVotingMembers()
+        public
+        view
+        returns (VotingMember[] memory)
+    {
+        uint256 length = 0;
+        for (uint256 i = 0; i < votingMembersInfoArray.length; i++) {
+            if (walletVotes[votingMembersInfoArray[i]].isOpen) {
+                length++;
             }
         }
-
-        VotingMember[] memory openVotingMembers = new VotingMember[](count);
+        VotingMember[] memory openVotingMembers = new VotingMember[](length);
         uint256 index = 0;
-        for (uint256 i = 0; i < VotingMembersInfoArray.length; i++) {
-            address walletAddress = VotingMembersInfoArray[i].walletAddress;
-            if (walletVotes[walletAddress].isOpen) {
-                VotingMember memory member;
-                member.walletAddress = walletAddress;
-                member.username = VotingMembersInfoArray[i].username;
-                member.yesVotes = walletVotes[walletAddress].yesVotes;
-                member.noVotes = walletVotes[walletAddress].noVotes;
-                openVotingMembers[index] = member;
+        for (uint256 i = 0; i < votingMembersInfoArray.length; i++) {
+            if (walletVotes[votingMembersInfoArray[i]].isOpen) {
+                openVotingMembers[index] = VotingMember(
+                    votingMembersInfoArray[i],
+                    walletVotes[votingMembersInfoArray[i]].nominateBy,
+                    walletVotes[votingMembersInfoArray[i]].username,
+                    walletVotes[votingMembersInfoArray[i]].yesVotes,
+                    walletVotes[votingMembersInfoArray[i]].noVotes,
+                    walletVotes[votingMembersInfoArray[i]].isManufacturer
+                );
                 index++;
             }
         }
         return openVotingMembers;
     }
 
-    function getAllWallets() public view returns (Wallet[] memory) {
-        return AllExistingMembers;
-    }
-        function getCompanySmartContractInfo(address _walletAddress) public view returns (ContractInfo[] memory) {
-        require(Iswalletexits(_walletAddress), "Wallet address does not exist");
-
-        return InfoOfProductAndContractAddress[_walletAddress];
-    }
-        function getSelfCompanySmartContractInfo() public view returns (ContractInfo[] memory) {
-        require(Iswalletexits(msg.sender), "Wallet address does not exist");
-
-        return InfoOfProductAndContractAddress[msg.sender];
-    }
-    function getAllProducts(address _contractAddress) public view returns (Company.Product[] memory) {
-        return Company(_contractAddress).getAllProductInformation();
+    function getAllManufacturers()
+        public
+        view
+        returns (OrganizationInfo[] memory)
+    {
+        return allManufacturers;
     }
 
+    function getAllSuppliers() public view returns (SupplierInfo[] memory) {
+        return allSuppliers;
+    }
+    function getAllProductInstances(
+        address _contractAddress
+    ) public view returns (Storage.ProductInfo[] memory) {
+        Storage storageInstance = Storage(_contractAddress);
+        return storageInstance.getall();
+    }
+
+    function getAllSelfProducts(
+        address _walletAddress
+    ) public view returns (ProductInfo[] memory) {
+        require(
+            manufacturerExists[_walletAddress].isTrue,
+            "Wallet address does not exist"
+        );
+
+        return manufacturerProducts[_walletAddress];
+    }
 }
